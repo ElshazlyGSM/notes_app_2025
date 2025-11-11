dd821ab07d7cc707e955143ae618e89f87387157
Ahmed Elshazly <elshazly.gsm@gmail.com>
2025-11-11 23:49:39 +0200

    repair error build apk

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app_2025/constants.dart';
+import 'package:notes_app_2025/models/notes_model.dart';
 import 'package:notes_app_2025/views/notes_view.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(NotesModelAdapter());
+
+  await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
 
